a007cca8fa0d462b1b516058ccef3428b90c1ada
Adam Duren <adamduren@gmail.com>
2015-03-13 16:12:49 -0700

    Add some documentation to the api and db

--- api.py
+++ api.py
@@ -6,14 +6,15 @@ from flask_cors import CORS
 
 from db import DataBase
 
-DATE_FORMAT = '%Y-%m-%d'
-JSON_DATE_FORMAT = '%Y-%m-%dT00:00:00Z'
+DATE_FORMAT = '%Y-%m-%d'  # Format for parsing api date params
+JSON_DATE_FORMAT = '%Y-%m-%dT00:00:00Z'  # Format for converting dates to JSON
 
 app = Flask(__name__)
-app.debug = False
+app.debug = True
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 
+# Map of metric_id's to friendly names
 metric_id_to_name_map = {
     'unique_visitors': 'Unique Visitors',
     'page_views': 'Page Views',
@@ -22,8 +23,29 @@ metric_id_to_name_map = {
 
 
 def ensure_data_for_missing_dates(data, start_date, end_date):
+    """
+    Checks the data for missing dates and fills gaps with zeros
+
+    Params
+    ------
+    data: [[datetime.date, int], ...]
+    start_date: datetime.date
+    end_date: datetime.date
+
+    Returns
+    -------
+    [int, ...]
+    """
+    # Get the difference between the start and end date in days
     days_delta = (end_date - start_date).days + 1
+
+    # Convert the data values to dicts where the key is the date
+    # and the value is the data for the date
     data = dict(data)
+
+    # Iterate over the expected dates
+    # use the value if present in the data dict
+    # otherwise default to zero
     data = [
         data.get(start_date + timedelta(days=date), 0)
         for date in xrange(days_delta)
@@ -33,9 +55,27 @@ def ensure_data_for_missing_dates(data, start_date, end_date):
 
 
 def transform_data_to_series(metric_id, data):
+    """
+    Prepares the metric and data to be sent as the API's response
+
+    Params
+    ------
+    metric_id: string
+        metric_id used to query the data
+    data: [int, ...]
+
+    Returns
+    -------
+    {
+        'name': string  # Friendly metric name
+        'total': int  # Sum of the data
+        'data': [int, ...] # The data that was passed in
+
+    }
+    """
     transformed_data = {
         'name': metric_id_to_name_map[metric_id],
-        # 'total': sum(data),
+        'total': sum(data),
         'data': data
     }
 
@@ -51,24 +91,24 @@ def api():
     delta_days = (end_date - start_date).days + 1
     db = DataBase()
 
-    api_response = {
+    response = {
         'index': [
             (start_date + timedelta(day)).strftime(JSON_DATE_FORMAT)
             for day in xrange(delta_days)
         ],
-        'series': {
-
-        }
+        'series': {}
     }
 
     for metric_id in metrics:
         data = db.get_data(metric_id, start_date, end_date)
         data = ensure_data_for_missing_dates(data, start_date, end_date)
+        data = transform_data_to_series(metric_id, data)
+        response['series'][metric_id] = data
 
-        api_response['series'][metric_id] = transform_data_to_series(metric_id, data)
-
-    return json.dumps(api_response, indent=4)
+    return json.dumps(response, indent=4)
 
 
+# Start the api if calling directly
+# $ python api.py
 if __name__ == '__main__':
     app.run()
--- db.py
+++ db.py
@@ -3,16 +3,58 @@ from random import sample, randint
 
 
 class DataBase(object):
+    """
+    Object emulating a database of metrics stored by dates_in_range
+
+    Example
+    -------
+    >>> from datetime import date
+    >>> db = DataBase()
+    >>> db.get_data('unique_visitors', date(2015, 1, 1), date(2015, 1, 15))
+    [[date(2015, 1, 1), 5], [date(2015, 1, 6), 5], [date(2015, 1, 9), 25]]
+
+    >>> db.get_data('invalid', date(2015, 1, 1), date(2015, 1, 15))
+    Exception: ValueError('Requested invalid column from database')
+    """
+
     valid_metrics = {'unique_visitors', 'page_views', 'visits'}
 
     def get_data(self, metric_id, start_date, end_date):
+        """
+        Returns data for the requested metric between the start and end date
+
+        Note: Data may not be available for all days
+
+        Params
+        ------
+        metric_id: string
+            one of the `valid_metrics`
+        start_date: datetime.date
+        end_date: datetime.date
+
+        Returns
+        -------
+        [[datetime.date, int], ...]
+        """
+
         if metric_id not in self.valid_metrics:
             raise ValueError('Requested invalid column from database')
 
+        # Get the distance in days between start and end date
         delta_days = (end_date - start_date).days + 1
-        dates = sample([
+
+        # Generate a list of all the dates in between start and end
+        dates_in_range = [
             (start_date + timedelta(day))
             for day in xrange(delta_days)
-        ], randint(0, delta_days))
+        ]
+        # Make a list by choosing a random number of dates from the list
+        # simulating that the database will not always have data
+        # for certain dates
+        dates_in_range = sample(dates_in_range, randint(0, delta_days))
 
-        return [[date, randint(0, 100)] for date in dates]
+        # Make rows of data for the dates were selected with the data being
+        # a random integer between 0 and 100
+        data = [[date, randint(0, 100)] for date in dates_in_range]
+
+        return data
